9d7c1473708e05c74fc36c3dcfcf544516f9d859
raaz4n <raazandoski@gmail.com>
2025-08-20 23:06:22 -0500

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
   console.log(`count is ${count}`)
   return (
-    <>
+    <>/*
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -15,7 +15,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </div>*/
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
