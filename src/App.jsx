0728fe1a2322d2aacfd1f730c5471571a8497426
raaz4n <raazandoski@gmail.com>
2025-08-21 13:57:33 -0500

    added notice about wip

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,9 @@ function App() {
           <a href="">projects</a>
         </div>
       </header>
+      <div className = "card">
+        <h1 style = {{color: "#27F549"}}>Website is a work in progress!</h1>
+      </div>
       <div>
         <img className = "photo" src = {myPhoto} alt = "small cat can't press any keys on the keyboard"/>
       </div>
