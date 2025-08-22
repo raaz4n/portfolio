4b40b6bec38c881f95db87934a3b1173209462d2
raaz4n <raazandoski@gmail.com>
2025-08-22 15:27:30 -0500

    using svgs now

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import myPhoto from './assets/small.jpg'
 import astromp4 from './assets/astro.mp4'
-import linkedIn from './assets/linkedin.png'
-import github from './assets/github.png'
+import linkedIn from './assets/linkedin.svg'
+import gitHub from './assets/github.svg'
 import './App.css'
 
 function App() {
@@ -13,11 +13,13 @@ function App() {
             <source src = {astromp4} type="video/mp4"/>
           </video>
           <p>/raaz4n</p>
-          <a href = "https://www.linkedin.com/in/raazan-doski-701a40257/">
+        </div>
+        <div>
+          <a href = "https://www.linkedin.com/in/raazan-doski-701a40257/" target = "_blank" rel = "nofollow norefferer">
             <img src = {linkedIn} width = "50" alt = "LinkedIn page"/>
           </a>
-          <a href = "https://github.com/raaz4n">
-            <img src = {github} width = "50" alt = "GitHub page"/>
+          <a href = "https://github.com/raaz4n" target = "_blank" rel = "nofollow noreferrer">
+            <img src = {gitHub} width = "50" alt = "GitHub page"/>
           </a>
         </div>
         <div className = "sections">
