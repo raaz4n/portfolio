95626ed7a97a3c4191e34ec91115cd863701b938
raaz4n <raazandoski@gmail.com>
2025-08-25 12:04:02 -0500

    Removed image on home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -14,9 +14,9 @@ export function Home() {
             <div className = "card">
                 <h1 style = {{color: "#27F549"}}>Website is a work in progress!</h1>
             </div>
-            <div>
+            {/*<div>
                 <img className = "photo" src = {myPhoto} alt = "small cat can't press any keys on the keyboard"/>
-            </div>
+            </div>*/}
         </>
     )
 }
