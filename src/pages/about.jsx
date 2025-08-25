b3b9690a6c7197cb6ee970492f0c3f3953422f6c
raaz4n <raazandoski@gmail.com>
2025-08-25 11:33:19 -0500

    changed about desc

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,11 +6,11 @@ export function About() {
             </div>
             <div className = "google-sans pagedesc">
                 <h3>
-                    Hi there! My name is Raazan. I am a developer aspiring to become
-                    a software engineer. Currently studying for my master's in Computer Science,
-                    I am very determined to make an impact in the field. At the top of this website,
-                    you can find links to my GitHub and LinkedIn if you are interested in learning
-                    more about me. You can also check out my projects and contact me directly.
+                    Hi there, I'm Raazan - a developer pursuing their Master's in Computer Science.
+                    I'm extremely passionate about building software that solves real world problems and am
+                    eager to grow into a full-time software role. On this website, you'll find 
+                    descriptions about my projects, ways to contact me, and links of my GitHub and LinkedIn.
+                    Feel free to reach out if you would like to connect!
                 </h3>
             </div>
         </>
